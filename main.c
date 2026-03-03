02ec91326e8660d561ef83770d0f59655a5ecfe3
Ranjitha051 <ranjithayashu05@gmail.com>
2026-03-03 13:20:46 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,42 +1,11 @@
-#include <stdio.h>
-#define SIZE 5
-int stack[SIZE];
-int top = -1;
-void push (int value) {
-if (top == SIZE -1)
-    printf("stack overflow! cannot push %d\n" , value);
-    else {
-        stack [++top] = value;
-    printf("%d pushed to stack\n" ,value);
-    }
-}
-void pop () {
-if (top == -1)
-    printf ("stack underflow! no element to pop\n");
-    else
-        printf("%d popped from stack\n", stack[top--]);
-}
-void display () {
-if (top== -1)
-    printf("stack is empty\n");
-    else {
-        printf("stack element:");
-        for (int i = top; i > 0; i--)
-            printf ("%d" , stack [i]);
-        printf ("\n");
-    }
-}
-int main () {
-push(10);
-push(20);
-push(30);
-display () ;
-pop();
-display ();
+#include<stdio.h>
+int main() {
+int arr[]={5,10,15,20,25};
+int*ptr=arr;
+printf("ptr=%p,*ptr=%d\n",ptr,*ptr);
+ptr++;
+printf("after ptr++; ptr=%p,*ptr%d\n",ptr,*ptr);
+ptr+=2;
+printf("after ptr+=2:ptr=%p,*ptr=%d\n",ptr,*ptr);
 return 0;
 }
-
-
-
-
-
