cd4d33339ee56082f2d729792cc93a2861dc2013
Ranjitha051 <ranjithayashu05@gmail.com>
2026-03-03 13:24:32 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,11 +1,34 @@
 #include<stdio.h>
-int main() {
-int arr[]={5,10,15,20,25};
-int*ptr=arr;
-printf("ptr=%p,*ptr=%d\n",ptr,*ptr);
-ptr++;
-printf("after ptr++; ptr=%p,*ptr%d\n",ptr,*ptr);
-ptr+=2;
-printf("after ptr+=2:ptr=%p,*ptr=%d\n",ptr,*ptr);
-return 0;
-}
+struct employee{
+int id;
+char name[50];
+    float salary;
+    };
+    int main() {
+    struct employee emp[5];
+    int i;
+    float totalsalary =0 ,avgsalary;
+    printf("enter details 0f 5 employees:\n");
+    for(i=0;i<5;i++) {
+        printf("\n empolyee %d\n",i+1);
+        printf("enter employee ID:");
+        scanf("%d",&emp[i].id);
+        printf("enter name:");
+        scanf("%s",emp[i].name);
+        printf("enter salary:");
+        scanf("%f",&emp[i].salary);
+        totalsalary+=emp[i].salary;
+    }
+    printf("\nemployee details:\n");
+    for(i=0;i<5;i++) {
+        printf("\n employee %d\n",i+1);
+        printf("ID:%d\n",emp[i].id);
+        printf("name:%s\n",emp[i].name);
+        printf("salary: %2f\n",emp[i].salary);
+    }
+    avgsalary = totalsalary / 5;
+    printf("\n average salary of 5 empolyees=%.2f\n",avgsalary);
+    return 0;
+    }
+
+
