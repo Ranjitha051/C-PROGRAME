f4875b030aaee0233270fd95823b2a1f678a982d
Ranjitha051 <ranjithayashu05@gmail.com>
2026-03-03 13:00:45 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-#include<string.h>
-int main() {
-char dest[10];
-char src[]="programming";
-//use strncpy to avoid buffer overflow strncpy(dest,src,size of (dest)-1);
-dest[sizeof(dest)-1]='\0';//ensure null terminate
-printf("safely copied string:%s",dest);
+int main()
+{
+
+    int a[5]={10,20,30,40,50};
+    for(int i=0;i<5;i++)
+        printf("%d",a[i]);
+    return 0;
 }
+
