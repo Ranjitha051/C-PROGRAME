06972a9e177028c6364d1b249c53b65d967f933a
Ranjitha051 <ranjithayashu05@gmail.com>
2026-03-03 13:18:42 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,42 @@
-#include<stdio.h>
-typedef struct {
-int emp_id;
-char name[10];
-float salary;
-}Employee;
-int main() {
-Employee e1={1002,"pallavi",30000.35};
-printf("Employee ID:%d\n",e1.emp_id);
-printf("Name:%s\n",e1.name);
-printf("Salary:%2f\n",e1.salary);
+#include <stdio.h>
+#define SIZE 5
+int stack[SIZE];
+int top = -1;
+void push (int value) {
+if (top == SIZE -1)
+    printf("stack overflow! cannot push %d\n" , value);
+    else {
+        stack [++top] = value;
+    printf("%d pushed to stack\n" ,value);
+    }
+}
+void pop () {
+if (top == -1)
+    printf ("stack underflow! no element to pop\n");
+    else
+        printf("%d popped from stack\n", stack[top--]);
+}
+void display () {
+if (top== -1)
+    printf("stack is empty\n");
+    else {
+        printf("stack element:");
+        for (int i = top; i > 0; i--)
+            printf ("%d" , stack [i]);
+        printf ("\n");
+    }
+}
+int main () {
+push(10);
+push(20);
+push(30);
+display () ;
+pop();
+display ();
 return 0;
 }
+
+
+
+
 
