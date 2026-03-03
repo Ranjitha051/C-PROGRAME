601f695b8ca4e3b1adea405bf166b8d4adfcba0c
Ranjitha051 <ranjithayashu05@gmail.com>
2026-03-03 13:01:57 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include<stdio.h>
-int main(void){
-int m[2][3]={{1,2,3},{4,5,6}};
-for(int i=0;i<2;i++)
-    for(int j=0;j<3;i++);
-    printf("%d%c",m[i][j],(j==2)?'\n':);
+struct cars{
+char name[20];
+char model[20];
+float prize;
+int cc;
+int manifacturing_date;
+};
+int main()
+{
+     struct cars c1={"bmw m series","bmw",5000000,2369,2003};
+           printf("name:%s\n",c1.name);
+           printf("model:%s\n",c1.model);
+           printf("prize:%2f\n",c1.prize);
+           printf("cc:%d\n",c1.cc);
+           printf("manifacturing_date:%d\n",c1.manifacturing_date);
+           printf("\n-------------------\n");
+
+           struct cars c2={"tata","nano",508598,2485,2008};
+               printf("name:%s\n",c2.name);
+               printf("model:%s\n",c2.model);
+               printf("prize:%2f",c2.prize);
+               printf("cc:%d\n",c2.cc);
+               printf("manifacturing_date:%d\n",c2.manifacturing_date);
+               printf("\n----------\n");
+
+
+
+           struct cars c3={"maruthi","suzuki",5655951,2548,2002};
+               printf("name:%s\n",c3.name);
+               printf("model:%s\n",c3.model);
+               printf("prize:%2f",c3.prize);
+               printf("cc:%d\n",c3.cc);
+               printf("manifacturing_date:%d\n",c3.manifacturing_date);
+               printf("\n-------------\n");
+
+
+
+           struct cars c4={"honda","civic",5548488,8655,2007};
+               printf("name:%s\n",c4.name);
+               printf("model:%s\n",c4.model);
+               printf("prize:%2f",c4.prize);
+               printf("cc:%d\n",c4.cc);
+               printf("manifacturing_date:%d\n",c4.manifacturing_date);
 return 0;
 }
